da033d810d3a863fc45453db5f31a3137c8216b9
ED-Develop <55874153+ED-Develop@users.noreply.github.com>
2019-11-26 21:21:10 +0100

    Updates

---
+++ precache-manifest.3ac1aa2864ddb8c491427d7f25c39ceb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "169948a9035ab97792a2c09c4a0b6105",
+    "url": "/online-store/index.html"
+  },
+  {
+    "revision": "445bc051cae70835b9ef",
+    "url": "/online-store/static/css/main.e0bae5ad.chunk.css"
+  },
+  {
+    "revision": "2f9accbd70e873bbdb06",
+    "url": "/online-store/static/js/2.d1e391e3.chunk.js"
+  },
+  {
+    "revision": "445bc051cae70835b9ef",
+    "url": "/online-store/static/js/main.456aa939.chunk.js"
+  },
+  {
+    "revision": "72c82a1a761ce6fecaaa",
+    "url": "/online-store/static/js/runtime-main.656a8bd8.js"
+  }
+]);
